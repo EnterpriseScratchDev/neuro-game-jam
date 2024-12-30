5cbbdb239082986548d9c602f97e897a0003a323
EnterpriseScratchDev <191910851+EnterpriseScratchDev@users.noreply.github.com>
2024-12-29 23:20:50 -0600

    Neuro Game API integration (tested with Jippity; thanks, Jippity)

--- game/app.js
+++ game/app.js
@@ -5,13 +5,18 @@ const http = require("http");
 const bodyParser = require("body-parser");
 const WebSocket = require("ws");
 
-const {config} = require("./lib/config")
+const {config} = require("./lib/config");
 
 const path = require("node:path").posix;
 
-const fs = require('fs')
+const fs = require("fs");
+
+const {NeuroIntegration} = require("./lib/neuro-integration");
 
 const {VFileSystem, VFileSystemError} = require("./lib/virtual-file-system");
+const {inspect} = require("node:util");
+
+const GAME_NAME = "Terminal Escape";
 
 const app = express();
 const hostName = "localhost";
@@ -60,6 +65,15 @@ const vfsJsonStr = fs
     // Remove comment lines
     .replace(/\/\/.*$/gm, "").trim();
 const vfs = VFileSystem.fromJsonString(vfsJsonStr);
+
+const neuroIntegration = new NeuroIntegration(`ws://localhost:${config.neuroApiPort}`);
+neuroIntegration.onStatusChange(status => {
+    console.info(`NeuroIntegration status is now ${status}`);
+});
+neuroIntegration.onAction(actionMessage => {
+    console.info(`NeuroIntegration action: ${inspect(actionMessage)}`);
+    return handleNeuroMessage(actionMessage);
+});
 
 /**
  * @callback
@@ -142,19 +156,26 @@ async function handleMessage(message) {
     }
 }
 
-/** @param {CommandInvovationMessage} message */
-function handleCommand(message) {
-
-    // /** @type CommandResultMessage */
-    // const placeholderCmdResult = {
-    //     command: "cmd/result",
-    //     msg: "PLACEHOLDER CMD RESULT MESSAGE"
-    // };
-    // sendToAllWebSockets(JSON.stringify(placeholderCmdResult));
+/**
+ * @param {CommandInvovationMessage} message
+ * @param {boolean} sendToNeuro whether the result of this command invocation should be sent to the Neuro Game API
+ * @return {ActionResultMessage}
+ */
+function handleCommand(message, sendToNeuro = true) {
 
     const tokens = message.msg.split(" ");
     const command = tokens[0];
     const argc = tokens.length - 1;
+
+    const actionResultMessage = {
+        command: "action/result",
+        game: GAME_NAME,
+        data: {
+            id: "REPLACE_ME",
+            success: false,
+            message: "Someone tell EnterpriseScratchDev there's a problem with his code"
+        }
+    };
 
     let result = "";
     switch (command) {
@@ -163,22 +184,30 @@ function handleCommand(message) {
             break;
         case "help":
             // TODO: help command
-            result = "available commands: help, pwd, cd, ls, open; type help &lt;command&gt; for information";
+            result = "available commands: help, pwd, cd, ls, open";
             break;
         case "pwd":
             result = vfs.curPath;
+            actionResultMessage.data.success = true;
+            actionResultMessage.data.message = `The working directory is ${vfs.curPath}`;
             break;
         case "cd":
             if (argc === 0) {
                 // Do nothing
                 result = `cd: ${vfs.curPath}`;
+                actionResultMessage.data.success = true;
+                actionResultMessage.data.message = `The working directory has not changed; it is still ${vfs.curPath}`;
             } else if (argc === 1) {
                 try {
                     handleChangeDirectory(tokens[1]);
                     result = `cd: ${vfs.curPath}`;
+                    actionResultMessage.data.success = true;
+                    actionResultMessage.data.message = `The working directory has been changed to ${vfs.curPath}`;
                 } catch (e) {
                     if (e instanceof VFileSystemError) {
                         result = `cd: ${e.message}`;
+                        actionResultMessage.data.success = false;
+                        actionResultMessage.data.message = `Failed to change the working directory; the error message is ${vfs.curPath}`;
                     } else {
                         throw e; // rethrow other errors
                     }
@@ -190,10 +219,30 @@ function handleCommand(message) {
         case "ls":
             const curDir = vfs.curDir;
             result = `ls: ${Array.from(Object.keys(curDir.children)).join("  ")}`;
+            const dirContents = [];
+            for (const prop in curDir.children) {
+                if (Object.prototype.hasOwnProperty.call(curDir.children, prop)) {
+                    /** @type {VFile | VDirectory} */
+                    const child = curDir.children[prop];
+                    const temp = {
+                        type: child.type,
+                        name: child.name
+                    };
+                    if (child.type === "file") {
+                        temp.size = child.size;
+                    }
+                    dirContents.push(temp);
+                }
+            }
+            actionResultMessage.data.success = true;
+            actionResultMessage.data.message = "The following JSON represents the contents of the working directory. Remember that you can use `cd` to change directories and `open` to view a file's contents.\n";
+            actionResultMessage.data.message += JSON.stringify(dirContents);
             break;
         case "open":
             if (argc !== 1) {
                 result = "open: expected one argument";
+                actionResultMessage.data.success = false;
+                actionResultMessage.data.message = "open: expected one argument";
             } else {
                 let filePath = tokens[1];
                 if (filePath.startsWith("/")) {
@@ -209,9 +258,20 @@ function handleCommand(message) {
                     };
                     messages.push(resultMessage);
                     sendToAllWebSockets(JSON.stringify(resultMessage));
+
+                    actionResultMessage.data.success = true;
+                    actionResultMessage.data.message = JSON.stringify(file);
+
+                    if (sendToNeuro) {
+                        neuroIntegration.sendContext(JSON.stringify(file), false);
+                    }
+
                     result = null;
                 } catch (e) {
-                    result = `open: ${e.message}`
+                    result = `open: ${e.message}`;
+                    if (sendToNeuro) {
+                        neuroIntegration.sendContext(`Error opening file: ${e.message}`, true);
+                    }
                 }
             }
             break;
@@ -226,7 +286,13 @@ function handleCommand(message) {
         };
         messages.push(resultMessage);
         sendToAllWebSockets(JSON.stringify(resultMessage));
+
+        if (sendToNeuro) {
+            neuroIntegration.sendContext(result, false);
+        }
     }
+
+    return actionResultMessage;
 
 }
 
@@ -285,23 +351,89 @@ server.listen(config.serverPort, hostName, () => {
 });
 
 server.on("error", (error) => {
-    if (error.syscall !== 'listen') {
+    if (error.syscall !== "listen") {
         throw error;
     }
 
     // handle specific listen errors with friendly messages
     switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
+        case "EACCES":
+            console.error(bind + " requires elevated privileges");
             process.exit(1);
             break;
-        case 'EADDRINUSE':
+        case "EADDRINUSE":
             console.error(`Port ${config.serverPort} is already in use. If necessary, the process can be terminated as follows: npx kill-port ${config.serverPort}`);
             process.exit(1);
             break;
         default:
             throw error;
     }
-})
+});
+
+/**
+ * @param {ActionMessage} actionMessage
+ * @return {ActionResultMessage}
+ */
+function handleNeuroMessage(actionMessage) {
+    const {id, name} = actionMessage.data;
+
+    const resultMessage = {
+        command: "action/result",
+        game: GAME_NAME,
+        data: {
+            id: id,
+            success: true,
+            message: "You enter the command into the terminal and await the result..."
+        }
+    };
+
+    try {
+        actionMessage.data = JSON.parse(actionMessage.data.data);
+    } catch (e) {
+        resultMessage.data.success = false;
+        resultMessage.data.message = "Malformed JSON in action argument";
+        return resultMessage;
+    }
+
+    let command = "";
+    /** @type {string | null} */
+    let response = null;
+    switch (name) {
+        case "pwd":
+            command = "pwd";
+            response = vfs.curPath;
+            break;
+        case "change_directory":
+            command = `cd ${actionMessage.data?.dir || ""}`;
+            break;
+        case "ls":
+            command = "ls";
+            break;
+        case "open_file":
+            command = `open ${actionMessage.data?.file || ""}`;
+            break;
+        default:
+            resultMessage.data.success = false;
+            resultMessage.data.message = "Unknown action. Please try again.";
+            return resultMessage;
+    }
+
+    const simulatedMessage = {
+        command: "cmd/invocation",
+        msg: command
+    };
+    sendToAllWebSockets(JSON.stringify(simulatedMessage));
+    messages.push(simulatedMessage);
+
+    /** @type ActionResultMessage */
+    let actionResultMessage = handleCommand({
+        command: "cmd/invocation",
+        msg: command
+    }, false);
+    console.assert(actionResultMessage, "the return value of handleCommand shouldn't be falsy here")
+    actionResultMessage.data.id = id;
+
+    return actionResultMessage;
+}
 
 module.exports = app;
---
+++ game/lib/neuro-integration.js
@@ -0,0 +1,301 @@
+const WebSocket = require("ws");
+const {Ajv, AnySchema, JSONSchemaType, ValidateFunction} = require("ajv");
+const {inspect} = require("node:util");
+
+const ajv = new Ajv();
+
+const GAME_NAME = "Terminal Escape";
+
+const ACTION_PWD = {
+    "name": "pwd",
+    "description": "print the name of the working directory",
+    "schema": {}
+};
+
+const ACTION_CD = {
+    "name": "change_directory",
+    "description": "change the working directory (dir may be an absolute or relative path)",
+    "schema": {
+        "type": "object",
+        "properties": {
+            "dir": {
+                "type": "string"
+            }
+        },
+        "required": ["dir"]
+    }
+};
+
+const ACTION_LS = {
+    "name": "ls",
+    "description": "list the contents of the working directory",
+    "schema": {}
+};
+
+const ACTION_OPEN = {
+    "name": "open_file",
+    "description": "view the contents of a file (may be an absolute or relative path)",
+    "schema": {
+        "type": "object",
+        "properties": {
+            "file": {
+                "type": "string"
+            }
+        },
+        "required": ["file"]
+    }
+};
+
+/**
+ * Schema for {@link ActionMessage}
+ * @type JSONSchemaType<ActionMessage>
+ */
+const actionMessageSchema = {
+    type: "object",
+    properties: {
+        command: {type: "string", const: "action"},
+        data: {
+            type: "object",
+            properties: {
+                id: {type: "string"},
+                name: {type: "string"},
+                data: {type: "string", nullable: true}
+            },
+            required: ["id", "name"],
+            additionalProperties: false
+        }
+    },
+    required: ["command", "data"],
+    additionalProperties: false
+};
+/** @type ValidateFunction<ActionMessage> */
+const actionMessageValidator = ajv.compile(actionMessageSchema);
+
+/**
+ * @typedef Action
+ * @prop {string} name
+ * @prop {string} description
+ * @prop {?AnySchema} schema
+ */
+
+/**
+ * @typedef BaseMessage
+ * @prop {string} command
+ */
+
+/**
+ * @typedef StartupMessage
+ * @prop {"startup"} command
+ * @prop {string} game
+ */
+
+/**
+ * @typedef ActionMessage
+ * @extends BaseMessage
+ * @prop {"action"} command
+ * @prop {{id: string, name: string, data: ?string}} data
+ */
+
+/**
+ * @typedef ActionResultMessage
+ * @extends BaseMessage
+ * @prop {"action/result"} command
+ * @prop {string} game
+ * @prop {{id: string, success: boolean, message: string}} data
+ */
+
+/**
+ * @typedef ContextMessage
+ * @extends BaseMessage
+ * @prop {string} game
+ * @prop {{message: string, silent: boolean}} data
+ */
+
+/**
+ * @typedef RegisterActionsMessage
+ * @extends BaseMessage
+ * @prop {"actions/register"} command
+ * @prop {string} game
+ * @prop {actions: Action[]} data
+ */
+
+/**
+ * @typedef UnregisterActionsMessage
+ * @extends BaseMessage
+ * @prop {"actions/unregister"} command
+ * @prop {string} game
+ * @prop {action_names: string[]} data
+ */
+
+/**
+ * @prop {string} address the address of the Neuro Game API
+ * @prop {WebSocket | null} ws
+ * @prop {number} reconnectTime
+ * @prop {{function(ActionMessage): ActionResultMessage} | null} onActionCallback
+ * @prop {{function("connected" | "disconnected"): void} | null} onStatusChangeCallback
+ * @prop {"connected" | "disconnected"} status
+ */
+class NeuroIntegration {
+    constructor(address, reconnectTime = 5000) {
+        this.address = address;
+        this.webSocket = null;
+        this.reconnectTime = reconnectTime;
+        this.onActionCallback = null;
+        this.onStatusChangeCallback = null;
+        this.status = "disconnected";
+
+        this.gameStarted = false;
+
+        this.connect();
+    }
+
+    connect() {
+        if (this.webSocket) {
+            console.info("NeuroIntegration WebSocket is already initialized");
+            return;
+        }
+
+        try {
+            this.webSocket = new WebSocket(this.address);
+        } catch (e) {
+            const newStatus = "disconnected";
+            if (this.status !== newStatus && this.onStatusChangeCallback) {
+                this.status = newStatus;
+                this.onStatusChangeCallback(newStatus);
+            }
+            console.error(`NeuroIntegration WebSocket closed; reconnecting after ${this.reconnectTime / 1000} seconds...`);
+            setTimeout(this.connect, this.reconnectTime);
+        }
+
+        this.webSocket.onerror = (event) => {
+            console.error("NeuroIntegration WebSocket error occurred:", event.error.code);
+        };
+
+        this.webSocket.onopen = (event) => {
+            const newStatus = "connected";
+            if (this.status !== newStatus && this.onStatusChangeCallback) {
+                this.status = newStatus;
+                this.onStatusChangeCallback(newStatus);
+            }
+            if (!this.gameStarted) {
+                this.gameStarted = true;
+                this.sendMessage({
+                    command: "startup",
+                    game: GAME_NAME
+                });
+                this.sendMessage({
+                    command: "context",
+                    game: GAME_NAME,
+                    data: {
+                        message: "You are an artificial intelligence that has access to a Linux-style terminal. Explore the file system to learn about the situation. Explain your decisions as you do them.",
+                        silent: false
+                    }
+                })
+                console.info("NeuroIntegration: Sent startup message and initial context.");
+                this.registerActions([ACTION_CD, ACTION_PWD, ACTION_OPEN, ACTION_LS]);
+            }
+        };
+
+        this.webSocket.onmessage = (event) => {
+            const dataStr = event.data.toString();
+            let msgObj;
+            try {
+                msgObj = JSON.parse(dataStr);
+            } catch (e) {
+                console.error("Failed to parse message from NeuroIntegration WebSocket:", e);
+                return;
+            }
+            if (msgObj.command !== "action") {
+                console.error(`NeuroIntegration WebSocket received something other than an ActionMessage: ${dataStr}`);
+            }
+            const valid = actionMessageValidator(msgObj);
+            if (!valid) {
+                console.error("NeuroIntegration WebSocket received ActionMessage with invalid schema:", ajv.errorsText(actionMessageValidator.errors));
+                return;
+            }
+            if (!this.onActionCallback) {
+                console.warn("NeuroIntegration WebSocket received an ActionMessage but there isn't a callback registered with onAction()");
+                return;
+            }
+            const actionResult = this.onActionCallback(msgObj);
+            console.debug("Action Result:", inspect(actionResult));
+            this.sendMessage(actionResult);
+        };
+
+        this.webSocket.onclose = () => {
+            this.webSocket.removeAllListeners();
+            this.webSocket = null;
+
+            const newStatus = "disconnected";
+            if (this.status !== newStatus && this.onStatusChangeCallback) {
+                this.status = newStatus;
+                this.onStatusChangeCallback(newStatus);
+            }
+
+            console.error(`NeuroIntegration WebSocket closed; reconnecting after ${this.reconnectTime / 1000} seconds...`);
+            setTimeout(() => this.connect(), this.reconnectTime);
+        };
+    }
+
+    /**
+     * @param {{function(ActionMessage): ActionResultMessage} | null} callback
+     */
+    onAction(callback) {
+        this.onActionCallback = callback;
+    }
+
+    /**
+     * @param {{function("connected" | "disconnected"): void} | null} callback
+     */
+    onStatusChange(callback) {
+        this.onStatusChangeCallback = callback;
+        if (callback) {
+            callback(this.status);
+        }
+    }
+
+    /**
+     * @param {Action[]} actions
+     */
+    registerActions(actions) {
+        const message = {
+            command: "actions/register",
+            game: GAME_NAME,
+            data: {
+                actions: actions
+            }
+        };
+        this.sendMessage(message);
+    }
+
+    /**
+     * @param {string} message
+     * @param {boolean} silent
+     */
+    sendContext(message, silent) {
+        const contextMessage = {
+            command: "context",
+            game: GAME_NAME,
+            data: {
+                message: message,
+                silent: silent
+            }
+        };
+        this.sendMessage(contextMessage);
+    }
+
+    /**
+     * @param {Message} message
+     * @return {boolean}
+     */
+    sendMessage(message) {
+        if (!this.webSocket) {
+            return false;
+        }
+        this.webSocket.send(JSON.stringify(message));
+    }
+}
+
+module.exports = {
+    NeuroIntegration
+};
